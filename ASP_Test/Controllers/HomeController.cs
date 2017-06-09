0ce45f44d021a8d5ce3d1c9f7132b3eb21121f40
MilleR <mr.oleg.melnyk@gmail.com>
2017-06-09 17:55:41 +0300

    Add project files.

---
+++ ASP_Test/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ASP_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ASP_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<User> users = new List<Models.User>() {
+            new User("FirstName1","LastName1","example1@example.com",new Post("LOL")),
+            new User("FirstName2","LastName2","example2@example.com",new Post("LOL")),
+            new User("FirstName3","LastName3","example3@example.com",new Post("LOL")),
+            new User("FirstName4","LastName4","example4@example.com",new Post("LOL")),
+            new User("FirstName5","LastName5","example5@example.com",new Post("LOL")) };
+
+        public ActionResult Index()
+        {
+            return View(users);
+        }
+
+        [HttpPost]
+        public ActionResult UserData()
+        {
+            users.Add(new User(Request.Form["FirstName"], Request.Form["LastName"], Request.Form["Email"], new Post(Request.Form["Title"])));
+            @ViewBag.Msg = "User added!";
+            return PartialView(users);
+        }
+
+        
+
+    }
+}
---
+++ ASP_Test/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_Test.Models
+{
+    public class User
+    {
+        public long ID { get;private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Post UserPost { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string fname, string lname,string email,Post post)
+        {
+            ID = BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 0);
+            FirstName = fname;
+            LastName = lname;
+            Email = email;
+            UserPost = post;
+        }
+
+
+    }
+}
