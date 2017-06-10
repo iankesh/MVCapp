8b195fd11a67e8c8a42251b34ed9457d0a541214
MilleR <mr.oleg.melnyk@gmail.com>
2017-06-11 00:38:35 +0300

    Initial commit.Created a model for network project.Service have an API for CRUD operation for current model.

--- ASP_Test/Controllers/HomeController.cs
+++ ASP_Test/Controllers/HomeController.cs
@@ -1,36 +1,111 @@
 ﻿using ASP_Test.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using System.Xml.Serialization;
 
 
 namespace ASP_Test.Controllers
 {
     public class HomeController : Controller
     {
-        private List<User> users = new List<Models.User>() {
-            new User("FirstName1","LastName1","example1@example.com",new Post("LOL")),
-            new User("FirstName2","LastName2","example2@example.com",new Post("LOL")),
-            new User("FirstName3","LastName3","example3@example.com",new Post("LOL")),
-            new User("FirstName4","LastName4","example4@example.com",new Post("LOL")),
-            new User("FirstName5","LastName5","example5@example.com",new Post("LOL")) };
+        private List<User> _users = new List<User>();
+
+        [XmlArray("UsersList")]
+        public List<User> Users {
+            get { return _users; }
+            set
+            {
+                if (value != null) _users = value;
+            }
+        }
+
+        private void Serializable(string xmlFileName, List<User> userList)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
+            using (FileStream fs = new FileStream(xmlFileName, FileMode.OpenOrCreate))
+            {
+                
+                serializer.Serialize(fs, userList);
+            }
+        }
+
+        private void Deserializable(string xmlFileName,List<User> userList)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<User>));
+            TextReader reader = new StreamReader(xmlFileName);
+            var enumerablelList = deserializer.Deserialize(reader) as List<User>;
+            if (enumerablelList != null)
+                foreach (var usr in enumerablelList)
+                {
+                    userList.Add(new User(usr.FirstName, usr.LastName, usr.Email));
+                    userList[userList.Count - 1].ID = usr.ID;
+                    
+                }
+
+            reader.Close();
+
+        }
 
         public ActionResult Index()
         {
-            return View(users);
+            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            return View(Users);
+        }
+
+        public ActionResult AddUser()
+        {
+            return View();
         }
 
         [HttpPost]
-        public ActionResult UserData()
+        public ActionResult AddUser(User userFromView)
         {
-            users.Add(new User(Request.Form["FirstName"], Request.Form["LastName"], Request.Form["Email"], new Post(Request.Form["Title"])));
-            @ViewBag.Msg = "User added!";
-            return PartialView(users);
+            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            Users.Add(new User(userFromView.FirstName,userFromView.LastName,userFromView.Email));
+            Serializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            return View("Index",Users);
         }
 
-        
+        [HttpPost]
+        public ActionResult EditUser(User userFromView)
+        {
+            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            var usr = Users.Find(u => u.ID == userFromView.ID);
+            usr.FirstName = userFromView.FirstName;
+            usr.LastName = userFromView.LastName;
+            usr.Email = userFromView.Email;
+            Serializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            return View("Index", Users);
+        }
 
+        public ActionResult EditUser(object id)
+        {
+            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            User usr = Users.Find(u => u.ID == Convert.ToInt64(id));
+            return View(usr);
+        }
+
+        [HttpPost]
+        public ActionResult RemoveUser(User userFromView)
+        {
+            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            var usr = Users.FirstOrDefault(u => u.ID == userFromView.ID);
+            Users.Remove(usr);
+            System.IO.File.WriteAllText(Server.MapPath("~/App_Data/UsersInfo.xml"), String.Empty);
+            Serializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            return View("Index", Users);
+        }
+
+        public ActionResult RemoveUser(object id)
+        {
+            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
+            User usr = Users.Find(u => u.ID == Convert.ToInt64(id));
+            return View(usr);
+        }
     }
 }
--- ASP_Test/Models/User.cs
+++ ASP_Test/Models/User.cs
@@ -2,29 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace ASP_Test.Models
 {
     public class User
     {
-        public long ID { get;private set; }
+        public long ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public Post UserPost { get; set; }
 
         public User()
         {
 
         }
 
-        public User(string fname, string lname,string email,Post post)
+        public User(string fname, string lname,string email)
         {
             ID = BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 0);
             FirstName = fname;
             LastName = lname;
             Email = email;
-            UserPost = post;
         }
 
 
