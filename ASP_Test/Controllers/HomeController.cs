00622575a328bdfb6cf9bf70ec9129be0a980ab8
MilleR <mr.oleg.melnyk@gmail.com>
2017-06-27 02:41:45 -0700

    df

--- ASP_Test/Controllers/HomeController.cs
+++ ASP_Test/Controllers/HomeController.cs
@@ -7,54 +7,20 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Xml.Serialization;
+using ASP_Test.DAL;
 
 
 namespace ASP_Test.Controllers
 {
     public class HomeController : Controller
     {
-        private List<User> _users = new List<User>();
+        private UnitOfWork unitOfWork = new UnitOfWork();
 
-        [XmlArray("UsersList")]
-        public List<User> Users {
-            get { return _users; }
-            set
-            {
-                if (value != null) _users = value;
-            }
-        }
-
-        private void Serializable(string xmlFileName, List<User> userList)
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(List<User>));
-            using (FileStream fs = new FileStream(xmlFileName, FileMode.OpenOrCreate))
-            {
-                
-                serializer.Serialize(fs, userList);
-            }
-        }
-
-        private void Deserializable(string xmlFileName,List<User> userList)
-        {
-            XmlSerializer deserializer = new XmlSerializer(typeof(List<User>));
-            TextReader reader = new StreamReader(xmlFileName);
-            var enumerablelList = deserializer.Deserialize(reader) as List<User>;
-            if (enumerablelList != null)
-                foreach (var usr in enumerablelList)
-                {
-                    userList.Add(new User(usr.FirstName, usr.LastName, usr.Email));
-                    userList[userList.Count - 1].ID = usr.ID;
-                    
-                }
-
-            reader.Close();
-
-        }
 
         public ActionResult Index()
         {
-            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            return View(Users);
+          
+            return View();
         }
 
         public ActionResult AddUser()
@@ -65,47 +31,34 @@ namespace ASP_Test.Controllers
         [HttpPost]
         public ActionResult AddUser(User userFromView)
         {
-            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            Users.Add(new User(userFromView.FirstName,userFromView.LastName,userFromView.Email));
-            Serializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            return View("Index",Users);
+           
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
         public ActionResult EditUser(User userFromView)
         {
-            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            var usr = Users.Find(u => u.ID == userFromView.ID);
-            usr.FirstName = userFromView.FirstName;
-            usr.LastName = userFromView.LastName;
-            usr.Email = userFromView.Email;
-            Serializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            return View("Index", Users);
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult EditUser(object id)
         {
-            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            User usr = Users.Find(u => u.ID == Convert.ToInt64(id));
-            return View(usr);
+
+            return View();
         }
 
         [HttpPost]
         public ActionResult RemoveUser(User userFromView)
         {
-            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            var usr = Users.FirstOrDefault(u => u.ID == userFromView.ID);
-            Users.Remove(usr);
-            System.IO.File.WriteAllText(Server.MapPath("~/App_Data/UsersInfo.xml"), String.Empty);
-            Serializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            return View("Index", Users);
+   ;
+            return RedirectToAction("Index");
         }
 
         public ActionResult RemoveUser(object id)
         {
-            Deserializable(Server.MapPath("~/App_Data/UsersInfo.xml"), Users);
-            User usr = Users.Find(u => u.ID == Convert.ToInt64(id));
-            return View(usr);
+            
+            return View();
         }
     }
 }
--- ASP_Test/Models/User.cs
+++ ASP_Test/Models/User.cs
@@ -8,24 +8,34 @@ namespace ASP_Test.Models
 {
     public class User
     {
-        public long ID { get; set; }
+        public long ID { get; private set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
 
         public User()
         {
 
         }
 
-        public User(string fname, string lname,string email)
+        public User(string fname, string lname,string email,string phone)
         {
             ID = BitConverter.ToInt64(Guid.NewGuid().ToByteArray(), 0);
             FirstName = fname;
             LastName = lname;
             Email = email;
+            PhoneNumber = phone;
         }
 
+        public User(long id,string fname, string lname, string email, string phone)
+        {
+            ID = id;
+            FirstName = fname;
+            LastName = lname;
+            Email = email;
+            PhoneNumber = phone;
+        }
 
     }
 }
